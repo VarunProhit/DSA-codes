3d017cf97d6766cca61c6038deeb921c735304a8
Varun <81059500+VarunProhit@users.noreply.github.com>
2022-08-31 23:11:57 +0530

    Time: 11 ms (12.65%), Space: 7.6 MB (78.76%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -8,30 +8,30 @@ class Solution
             m = o[0].size();
             int dp[n][m];
             if (o[0][0] == 0)
-                dp[0][0] = 1;
-            else dp[0][0] = 0;
+                o[0][0] = 1;
+            else o[0][0] = 0;
             for (i = 1; i < m; i++)
             {
-                if (o[0][i] != 1) dp[0][i] = dp[0][i - 1];
-                else dp[0][i] = 0;
+                if (o[0][i] != 1) o[0][i] = o[0][i - 1];
+                else o[0][i] = 0;
             }
             for (i = 1; i < n; i++)
             {
-                if (o[i][0] != 1) dp[i][0] = dp[i - 1][0];
-                else dp[i][0] = 0;
+                if (o[i][0] != 1) o[i][0] = o[i - 1][0];
+                else o[i][0] = 0;
             }
             for (i = 1; i < n; i++)
             {
                 for (j = 1; j < m; j++)
                 {
                     if (o[i][j] == 1)
-                        dp[i][j] = 0;
+                        o[i][j] = 0;
                     else
                     {
-                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                        o[i][j] = o[i - 1][j] + o[i][j - 1];
                     }
                 }
             }
-            return dp[n - 1][m - 1];
+            return o[n - 1][m - 1];
         }
 };
