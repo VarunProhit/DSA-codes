3115c3de077fc0f8f65a55eb61b37b4ff75355d7
Varun <81059500+VarunProhit@users.noreply.github.com>
2022-09-02 10:30:25 +0530

    Time: 30 ms (32.77%), Space: 22.4 MB (72.83%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -1,38 +1,40 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<double> averageOfLevels(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
-        vector<double> ans;
-        while(!q.empty())
+class Solution
+{
+    public:
+        vector<double> averageOfLevels(TreeNode *root)
         {
-            double a=0.0;
-            int n=q.size();
-            int k=n;
-            while(n--)
+            queue<TreeNode*> q;
+            q.push(root);
+            vector<double> ans;
+            while (!q.empty())
             {
-               auto m = q.front();
-                q.pop();
-                if(m->left)
-                    q.push(m->left);
-                if(m->right)
-                    q.push(m->right);
-                a+= m->val;
+                double a = 0.0;
+                int n = q.size();
+                int k = n;
+                while (n--)
+                {
+                    auto m = q.front();
+                    q.pop();
+                    if (m->left)
+                        q.push(m->left);
+                    if (m->right)
+                        q.push(m->right);
+                    a += m->val;
+                }
+                a = (a *1.0) / (k *1.0);
+                ans.push_back(a);
             }
-            a= (a*1.0)/(k*1.0);
-           ans.push_back(a);            
+            return ans;
         }
-        return ans;
-    }
 };
