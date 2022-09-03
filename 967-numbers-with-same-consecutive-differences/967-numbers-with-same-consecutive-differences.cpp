d0ad153b792479183b39d131e2837ecd6c0ae260
Varun <81059500+VarunProhit@users.noreply.github.com>
2022-09-03 07:32:32 +0530

    Time: 6 ms (42.98%), Space: 9.6 MB (11.96%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,31 +1,31 @@
-class Solution {
-public:
-    void ck(string a,int i,int n,int k, set<string> &ans)
-    {
-        if(i<0)
-            return;
-        if(i>9)
-            return;
-        a+=i+'0';
-         if(a.size()==n)
+class Solution
+{
+    public:
+        void ck(string a, int i, int n, int k, set<string> &ans)
         {
-            ans.insert(a);
-            return;
+            if (i < 0)
+                return;
+            if (i > 9)
+                return;
+            a += i + '0';
+            if (a.size() == n)
+            {
+                ans.insert(a);
+                return;
+            }
+            ck(a, i + k, n, k, ans);
+            ck(a, i - k, n, k, ans);
         }
-        ck(a,i+k,n,k,ans);
-        ck(a,i-k,n,k,ans);
-        
-    }
-    vector<int> numsSameConsecDiff(int n, int k) {
+    vector<int> numsSameConsecDiff(int n, int k)
+    {
         string a;
-       set<string> ans;
+        set<string> ans;
         vector<int> an;
-        for(int i=1;i<=9;i++)
+        for (int i = 1; i <= 9; i++)
         {
             a.clear();
-            //a+=i+'0';
-            ck(a,i,n,k,ans);
-            for(auto x:ans)
+            ck(a, i, n, k, ans);
+            for (auto x: ans)
             {
                 an.push_back(stoi(x));
             }
