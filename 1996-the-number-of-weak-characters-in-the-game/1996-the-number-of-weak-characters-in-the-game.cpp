7d295867c0c3dc1b4f07b6d0d0e6dedfcdfdee8f
Varun <81059500+VarunProhit@users.noreply.github.com>
2022-09-09 14:47:45 +0530

    Time: 1687 ms (5.05%), Space: 125.2 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
-    static bool comp(vector<int> &a,vector<int> &b){
-        if(a[0]!=b[0])
-            return a[0]>b[0];
-        return a[1]<b[1];
+    
+    // we will sort the array on the basis of attack in increasing order
+    
+    // when attack will be equal then we will sort on the basis of defence in decreasing order
+    
+    static bool compare(vector<int>& a, vector<int>& b)
+    {
+        if(a[0] == b[0])
+            return a[1] > b[1];
+        else
+            return a[0] < b[0];
     }
-    int numberOfWeakCharacters(vector<vector<int>>& prop) {
-        sort(prop.begin(),prop.end(),comp);
-        int maxTillNow = INT_MIN;
-        int ans=0;
-        for(auto p:prop){
-            if(maxTillNow>p[1]) ans++;
-            else maxTillNow=p[1];
+    
+    int numberOfWeakCharacters(vector<vector<int>>& arr) {
+        
+        int n = arr.size();
+        
+        // sort the arr
+        
+        sort(arr.begin(), arr.end(), compare);
+        
+        int count = 0;
+        
+        // max_defence will keep track of maximum defence from last
+        
+        int max_defence = arr[n - 1][1];
+        
+        for(int i = n - 2; i >= 0; i--)
+        {
+		    // if curr_defence is less than max_defence
+			
+            if(arr[i][1] < max_defence)
+            {
+                count++;
+            }
+            
+            // update max_defence
+            
+            max_defence = max(max_defence, arr[i][1]);
         }
-        return ans;
+        
+        return count;
     }
 };
