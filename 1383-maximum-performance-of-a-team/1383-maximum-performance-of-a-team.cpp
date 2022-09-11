a89f52e2c5a5633bb480902f71172ea0fd3d485d
Varun <81059500+VarunProhit@users.noreply.github.com>
2022-09-11 07:51:14 +0530

    Time: 221 ms (15.67%), Space: 36.8 MB (74.47%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,45 +1,24 @@
-class Solution
-{
-    public:
-        int maxPerformance(int n, vector<int> &speed, vector<int> &efficiency, int k)
+class Solution {
+public:
+    int maxPerformance(int n, vector<int>& a, vector<int>& b, int k) 
+    {
+        vector<pair<int,int>> v;
+        for(int i=0;i<n;i++)
+            v.push_back({b[i],a[i]});
+        sort(v.begin(),v.end(),greater<pair<int,int>>());
+       
+        priority_queue<int,vector<int>,greater<int>> q;
+        long sum=0,ans=0,mod=1e9+7;
+        for(int i=0;i<n;i++)
         {
-            vector<vector < int>> ef(n, vector<int> (2));
-            for (int i = 0; i < n; i++)
-            {
-                ef[i][0] = efficiency[i];
-                ef[i][1] = speed[i];
-            }
-           	// sort by efichency
-            sort(ef.begin(), ef.end(), [](vector<int> &a, vector<int> &b)
-            {
-                if (a[0] != b[0]) return a[0] > b[0];
-                return a[1] > b[1];
-	});
-            priority_queue<int, vector < int>, greater < int>> q;
-            long long sum = 0;
-            int ef_min = 0;
-            long long maxi = 0;
-            for (auto &e: ef)
-            {
-                if (q.size() < k)
-                {
-                	// Calc max for first k
-                    sum = (sum + (long long) e[1]);
-                    ef_min = e[0];
-                    q.push(e[1]);
-                    maxi = max(maxi, (sum *(long long) ef_min));
-                }
-                else
-                {
-                	// Going down with efichency update the best k preforming sum
-                    if (e[1] <= q.top()) continue;
-                    sum = (sum - (long long) q.top() + (long long) e[1]);	//  best k preforming sum
-                    q.pop();
-                    q.push(e[1]);
-                    ef_min = e[0];
-                    maxi = max(maxi, (sum *(long long) ef_min));	// compare to max
-                }
-            }	// cout << e[0] << endl;
-            return maxi % 1000000007;
+            q.push(v[i].second);
+            sum+=v[i].second;
+            
+            if(q.size()>k)
+                sum-=q.top(),q.pop();
+            
+            ans=max(ans,sum*v[i].first);
         }
+        return (int)(ans%mod);
+    }
 };
