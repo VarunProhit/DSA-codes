4f48a28a5aea7b51fddba4e5c0bc116071acbddb
Varun <81059500+VarunProhit@users.noreply.github.com>
2022-09-20 08:30:55 +0530

    Time: 290 ms (85.33%), Space: 16 MB (81.84%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,21 +1,24 @@
-class Solution {
-public:
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-       int n= nums1.size();int m = nums2.size();
-        int dp[n+1][m+1];
-        int i,j,k;
-        k=0;
-        memset(dp,0,sizeof(dp));
-        for(i=1;i<=n;i++)
+class Solution
+{
+    public:
+        int findLength(vector<int> &nums1, vector<int> &nums2)
         {
-            for(j=1;j<=m;j++)
+            int n = nums1.size();
+            int m = nums2.size();
+            int dp[n + 1][m + 1];
+            int i, j, k;
+            k = 0;
+            memset(dp, 0, sizeof(dp));
+            for (i = 1; i <= n; i++)
             {
-                if(nums1[i-1]!=nums2[j-1])
-                    dp[i][j]=0;
-                else dp[i][j]= 1+dp[i-1][j-1];
-                k = max(k,dp[i][j]);
+                for (j = 1; j <= m; j++)
+                {
+                    if (nums1[i - 1] != nums2[j - 1])
+                        dp[i][j] = 0;
+                    else dp[i][j] = 1 + dp[i - 1][j - 1];
+                    k = max(k, dp[i][j]);
+                }
             }
+            return k;
         }
-        return k;
-    }
 };
