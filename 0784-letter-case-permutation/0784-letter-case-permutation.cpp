71253eed1b017b759f3eb94aa22c99473f6e9d2a
Varun <81059500+VarunProhit@users.noreply.github.com>
2023-02-07 09:03:15 +0530

    Time: 4 ms (87.70%), Space: 11.8 MB (28.57%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,46 +1,32 @@
 class Solution
 {
-  void permutation (string s, string output, int i, vector < string > &ans)
-  {
-    //base case
-    if (s.length () == i)
-      {
-	ans.push_back (output);
-	return;
-      }
+    public:
+        void solve(vector<string> &ans, string s, int i)
+        {
+            if (i == s.size())
+            {
+                ans.push_back(s);
+                return;
+            }
 
-    if (isdigit (s[i]))
-      {
-     output.push_back (s[i]);
-	permutation (s, output, i + 1, ans);
-      }
-    else
-      {
-	output.push_back (s[i]);
-	permutation (s, output, i + 1, ans);
-	output.pop_back ();
-	
-	char a = s[i];
-	if (islower (a))
-	  {
-	    output.push_back (toupper (s[i]));
-	    permutation (s, output, i + 1, ans);
-	  }
-	else
-	  {
-	    char b = s[i];
-	    output.push_back (tolower (s[i]));
-	    permutation (s, output, i + 1, ans);
-  }
-  }
-  }
-public:
-  vector < string > letterCasePermutation (string s)
-  {
-    vector < string > ans;
-    string output = "";
-    int i = 0;
-    permutation (s, output, i, ans);
-    return ans;
-  }
+            if (s[i] >= 'A' && s[i] <= 'Z')
+            {
+                s[i] = tolower(s[i]);
+                solve(ans, s, i + 1);
+                s[i] = toupper(s[i]);
+            }
+            else if (s[i] >= 'a' && s[i] <= 'z')
+            {
+                s[i] = toupper(s[i]);
+                solve(ans, s, i + 1);
+                s[i] = tolower(s[i]);
+            }
+            solve(ans, s, i + 1);
+        }
+    vector<string> letterCasePermutation(string s)
+    {
+        vector<string> ans;
+        solve(ans, s, 0);
+        return ans;
+    }
 };
