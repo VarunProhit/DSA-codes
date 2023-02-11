8c390578daecc5f75ddf57948decfcc1abce16cb
Varun <81059500+VarunProhit@users.noreply.github.com>
2023-02-11 09:24:41 +0530

    Added solution - LeetHub

---
+++ Product array puzzle - GFG/product-array-puzzle.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+       
+        //code here    
+        vector<long long int> ans;
+        long long int pr=1,cn=0;
+        for(auto x: nums){
+            if(x!=0)pr*=x; 
+            else cn++;
+            
+        }
+        for(auto x:nums)
+        {
+            if(x!=0 && cn==0)ans.push_back(pr/x);
+            else if(x==0 && cn==1)ans.push_back(pr);
+            else ans.push_back(0ll);
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;  // number of test cases
+    cin>>t;
+    while(t--)
+    {
+        int n;  // size of the array
+        cin>>n;
+        vector<long long int> arr(n),vec(n);
+        
+        for(int i=0;i<n;i++)    // input the array
+        {
+            cin>>arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr,n);   // function call
+        
+        for(int i=0;i<n;i++)    // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
