5f9d48f8757173390b1cf131ae981a3408551e2f
Varun <81059500+VarunProhit@users.noreply.github.com>
2023-02-18 19:23:46 +0530

    Time: 10 ms (50.91%), Space: 13.1 MB (65.66%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int profit=0;
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i]<arr[i+1])
+                profit+=(arr[i+1]-arr[i]);
+        }
+        return profit;
+    }
+};
