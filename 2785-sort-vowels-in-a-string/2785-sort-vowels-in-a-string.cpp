0f55a172fae21ebf9b51a8e82fcadf098b6a784f
Varun <81059500+VarunProhit@users.noreply.github.com>
2023-11-13 10:06:03 +0530

    Time: 51 ms (25.81%), Space: 13.2 MB (49.92%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,22 +1,60 @@
+// class Solution {
+// public:
+//     string sortVowels(string s) {
+//         vector<pair<int,char>> v;
+//         for(auto x:s){
+//          if(x=='a' || x=='e'||x=='i' || x=='o'||x=='u' || x=='A'||x=='E' || x=='I'||x=='O' || x=='U'){
+//            v.push_back({x,x});
+//          }
+//         }
+//         sort(v.begin(),v.end());
+        
+//         int i=0;
+//         for(int j=0;j<s.size();j++){
+//             auto x = s[j];
+//              if(x=='a' || x=='e'||x=='i' || x=='o'||x=='u' || x=='A'||x=='E' || x=='I'||x=='O' || x=='U'){
+//                 s[j]=v[i].second;
+//                  i++;
+//          }
+//         }
+//         return s;
+//     }
+// };
 class Solution {
 public:
+    // Returns true if the character is a vowel.
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'o'|| c == 'u'|| c == 'i' 
+            || c == 'A' || c == 'E' || c == 'O'|| c == 'U'|| c == 'I';
+    }
+    
     string sortVowels(string s) {
-        vector<pair<int,char>> v;
-        for(auto x:s){
-         if(x=='a' || x=='e'||x=='i' || x=='o'||x=='u' || x=='A'||x=='E' || x=='I'||x=='O' || x=='U'){
-           v.push_back({x,x});
-         }
+        unordered_map<char, int> count;
+
+        // Store the frequencies for each character.
+        for (char c : s) {
+            if (isVowel(c)) {
+                count[c]++;
+            }
         }
-        sort(v.begin(),v.end());
-        
-        int i=0;
-        for(int j=0;j<s.size();j++){
-            auto x = s[j];
-             if(x=='a' || x=='e'||x=='i' || x=='o'||x=='u' || x=='A'||x=='E' || x=='I'||x=='O' || x=='U'){
-                s[j]=v[i].second;
-                 i++;
-         }
+
+        // Sorted string having all the vowels.
+        string sortedVowel = "AEIOUaeiou";
+        string ans;
+        int j = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (!isVowel(s[i])) {
+                ans += s[i];
+            } else {
+                // Skip to the character which is having remaining count.
+                while (count[sortedVowel[j]] == 0) {
+                    j++;
+                }
+
+                ans += sortedVowel[j];
+                count[sortedVowel[j]]--;
+            }
         }
-        return s;
+        return ans;
     }
 };
