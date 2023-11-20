f0269ef4e94173f89ea44f8e531267fc5ba65caf
Varun <81059500+VarunProhit@users.noreply.github.com>
2023-11-20 19:09:46 +0530

    Time: 144 ms (92.59%), Space: 83.1 MB (68.21%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,21 +1,39 @@
+// class Solution {
+// public:
+//     int reductionOperations(vector<int>& nums) {
+        
+//         map<int,int> m;
+//         for(auto x:nums){
+//             m[x]++;
+//         }
+//         sort(nums.begin(),nums.end());
+//         int ans=0,temp=0;
+//         for(int i=nums.size()-1;i>0;i--){
+//             if(m[nums[i]]>0 && nums[i]!=nums[0]){
+//                 ans+=m[nums[i]] + temp;
+//                temp +=m[nums[i]];
+//                m[nums[i]]=0;
+                
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int up = 0;
         
-        map<int,int> m;
-        for(auto x:nums){
-            m[x]++;
-        }
-        sort(nums.begin(),nums.end());
-        int ans=0,temp=0;
-        for(int i=nums.size()-1;i>0;i--){
-            if(m[nums[i]]>0 && nums[i]!=nums[0]){
-                ans+=m[nums[i]] + temp;
-               temp +=m[nums[i]];
-               m[nums[i]]=0;
-                
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                up++;
             }
+            
+            ans += up;
         }
+        
         return ans;
     }
 };
