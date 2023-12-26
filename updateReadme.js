5c98317303f8900e397d05221652982f412c7f5a
Varun <81059500+VarunProhit@users.noreply.github.com>
2023-12-26 22:45:32 +0530

    remove unwanted file from readme

--- updateReadme.js
+++ updateReadme.js
@@ -1,20 +1,34 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // Function to update the README file
 function updateReadme() {
-  const files = fs.readdirSync('./'); // List files in the root directory
-  const updatedContent = generateReadmeContent(files);
+	const files = fs.readdirSync("./"); // List files in the root directory
+	const updatedContent = generateReadmeContent(files);
 
-  // Update README.md with the generated content
-  fs.writeFileSync('README.md', updatedContent);
+	// Update README.md with the generated content
+	fs.writeFileSync("README.md", updatedContent);
 }
 
 // Function to generate README content
 function generateReadmeContent(files) {
-  const fileList = files.filter(file => file !== 'README.md'); // Exclude README.md
-  const formattedFileList = fileList.map(file => `- ${file}`).join('\n');
-
-  return `# Project Repository
+	const fileList = files.filter(
+		(file) =>
+			![
+        ".git",
+				".gitattributes",
+				".gitignore",
+				".github",
+				".vscode",
+				"node_modules",
+				"package.json",
+				"package-lock.json",
+				"updateReadme.js",
+				"README.md",
+				"LICENSE",
+			].includes(file)
+	); // Exclude README.md
+  const formattedFileList = fileList.map((file) => `- [${file}](./${file})`).join("\n");
+	return `# Project Repository
 
 ## Files and Folders
 ${formattedFileList}
