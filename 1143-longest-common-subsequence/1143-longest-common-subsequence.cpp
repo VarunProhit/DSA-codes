a06015e21b2472d20fa0e328b0d49fdb835b0e80
Varun <81059500+VarunProhit@users.noreply.github.com>
2024-01-25 16:46:33 +0530

    Time: 12 ms (99.11%), Space: 11.9 MB (71.44%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,53 +1,24 @@
 class Solution {
 public:
-    int lcs(string X, string Y, int m, int n)
-{
- 
-    // Initializing a matrix of size
-    // (m+1)*(n+1)
-    int L[m + 1][n + 1];
- 
-    // Following steps build L[m+1][n+1]
-    // in bottom up fashion. Note that
-    // L[i][j] contains length of LCS of
-    // X[0..i-1] and Y[0..j-1]
-    for (int i = 0; i <= m; i++) {
-        for (int j = 0; j <= n; j++) {
-            if (i == 0 || j == 0)
-                L[i][j] = 0;
- 
-            else if (X[i - 1] == Y[j - 1])
-                L[i][j] = L[i - 1][j - 1] + 1;
- 
-            else
-                L[i][j] = max(L[i - 1][j], L[i][j - 1]);
-        }
-    }
- 
-    // L[m][n] contains length of LCS
-    // for X[0..n-1] and Y[0..m-1]
-    return L[m][n];
-}
- 
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
         int dp[n+1][m+1];
         int i,j;
-        // for(int i=0;i<=n;i++)
-        //     dp[i][0]=0;
-        // for(int i=0;i<=m;i++)
-        //     dp[0][i]=0;
-        // for(i=1;i<=n;i++)
-        // {
-        //     for(j=1;j<=m;j++)
-        //     {
-        //         if(text1[i-1]==text2[j-1])
-        //             dp[i][j] = dp[i-1][j-1]+1;
-        //         else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-        //     }
-        // }
-        return lcs(text1,text2,n,m);
+        for(int i=0;i<=n;i++)
+            dp[i][0]=0;
+        for(int i=0;i<=m;i++)
+            dp[0][i]=0;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                    dp[i][j] = dp[i-1][j-1]+1;
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
     
 };
