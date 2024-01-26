b66620be811ebb9374da384f1bfa4e5df142e807
Varun <81059500+VarunProhit@users.noreply.github.com>
2024-01-26 16:22:09 +0530

    Time: 150 ms (94.72%), Space: 131.7 MB (5.16%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        
+        vector<int> v(n+1,1);
+       
+        for(int i=2;i*i<=n;i++)
+        {
+           if(v[i]==1)
+           {
+               for(int j=i*i;j<=n;j+=i)v[j]=0;
+           }
+        }
+        int ans=0;
+        for(int i=2;i<n;i++)if(v[i])ans++;
+        return ans;
+    }
+};
