3bca90005ee1595d0147dec924ef86b395429e38
Varun <81059500+VarunProhit@users.noreply.github.com>
2024-02-02 10:56:21 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (9.62%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        
-        string s = to_string(low);
-        string t = to_string(high);
-        vector<int> ans;
-        int i,j,k;
-        for(i=s.size();i<=t.size();i++)
-        {
-            string add;
-            while(add.size()<i)add+='1';
-            string st;
-            for(j=1;j<=9;j++)
-            {
-                if(st.size()<i)st+=j+'0';
-                else break;
+        std::vector<int> result;
+
+        for (int i = 1; i <= 9; i++) {
+            int num = i;
+
+            for (int j = i + 1; j <= 9; j++) {
+                num = num * 10 + j;
+
+                if (num >= low && num <= high) {
+                    result.push_back(num);
+                }
             }
-            while(st.size()==i)
-            {
-                if(stoi(st)>=low && stoi(st)<=high)
-                  ans.push_back(stoi(st));
-                if(st[st.size()-1]=='9')break;
-                st = to_string(stoi(st)+stoi(add));
-            }
-            
         }
-        return ans;
-        
+
+        std::sort(result.begin(), result.end());
+        return result;
     }
 };
