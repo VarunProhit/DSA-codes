202bc48fe5022daba264a3fd9098e7639b115e4e
Varun <81059500+VarunProhit@users.noreply.github.com>
2024-08-01 08:43:11 +0530

    Time: 13 ms (5.72%), Space: 17.5 MB (31.37%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        
+        int ans=0;
+        for(auto x:details){
+            int age = (x[11]-'0')*10+(x[12]-'0');
+            if(age>60)ans++;
+        }
+        return ans;
+    }
+};
